a3d9c98d2ca975be2220554782c6564dfdcf1c28
Ankit Keshari <GS-1544@GSLAB.COM>
2023-09-16 09:33:34 +0530

    Modified user modal

--- _helpers/const.js
+++ _helpers/const.js
@@ -3,27 +3,32 @@ const STATUS = {
   FAILED: "Failed",
 };
 
-const JIRA_STATUS={
-  TODO:"TODO",
-  INPROGRESS:"IN-PROGRESS",
-  DONE:"DONE"
-}
+const JIRA_STATUS = {
+  TODO: "TODO",
+  INPROGRESS: "IN-PROGRESS",
+  DONE: "DONE",
+};
 
-
-const ROLE_NAME={
-ENTERPRISE_ADMIN:  "Enterprise Admin",
-REGULAR_ENTERPRISE:"Regular Enterprise",
-REGULAR_USER:"Regular User"
-}
+const ROLE_NAME = {
+  ENTERPRISE_ADMIN: "Enterprise",
+  REGULAR_ENTERPRISE: "Basic",
+  REGULAR_USER: "Regular User",
+};
 const QUICK_PULSE_CHECK_QUESTIONS = [
-  '1. People & Resources ',
-  '2. Work Processes ',
-  '3. Technical Tools ',
-]
-const RETRO_STATUS={
-  WAITING:'waiting',
-  STARTED:'started',
-  ENDED:'ended'
-}
+  "1. People & Resources ",
+  "2. Work Processes ",
+  "3. Technical Tools ",
+];
+const RETRO_STATUS = {
+  WAITING: "waiting",
+  STARTED: "started",
+  ENDED: "ended",
+};
 
-module.exports = { STATUS,JIRA_STATUS,ROLE_NAME,QUICK_PULSE_CHECK_QUESTIONS,RETRO_STATUS };
+module.exports = {
+  STATUS,
+  JIRA_STATUS,
+  ROLE_NAME,
+  QUICK_PULSE_CHECK_QUESTIONS,
+  RETRO_STATUS,
+};
